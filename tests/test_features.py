2f5b1146b05eec521263461504a76d97ea3a5b59
aipusulaofficial-cyber <aipusulaofficial@gmail.com>
2026-09-25 14:58:57 +0300

    feat: implement versioned TTL feature store

---
+++ tests/test_features.py
@@ -0,0 +1,9 @@
+from feature_store import *
+import pytest
+def test_versioned_read():
+ s=FeatureStore();s.put(Feature("risk",1,.7));s.put(Feature("risk",2,.8));assert s.get("risk",2)==.8
+def test_ttl():
+ s=FeatureStore();s.put(Feature("x",1,1,10))
+ with pytest.raises(KeyError):s.get("x",1,10)
+def test_missing():
+ with pytest.raises(KeyError):FeatureStore().get("x",1)
